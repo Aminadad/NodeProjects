b154d06de159225ff40a9d401accbc86941c60b3
Aminadad <aminadad77@hotmail.com>
2023-03-17 22:30:42 -0400

    modify

--- models/mascota.js
+++ models/mascota.js
@@ -1,4 +1,19 @@
 const mongoose = require('mongoose');
+const URI = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@clusteraminadad.j8lmrfp.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+
+//Coneccion a bd
+main();
+
+async function main(){
+try{
+  await mongoose.connect(URI);
+  await console.log('Coneccion establecida');
+}catch(err){
+    console.log(err);
+}
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
 const Schema = mongoose.Schema;
 
 const mascotaSchema = new Schema({
--- routes/mascotas.js
+++ routes/mascotas.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mascota = require('../models/mascota');
+
 const Mascota = require('../models/mascota');
 
 
