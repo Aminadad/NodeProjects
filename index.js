da985b7fb3e50ee34178fa77d20a072de4b0c643
Aminadad <aminadad77@hotmail.com>
2023-03-18 13:26:29 -0400

    modify5

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+
 
 //Coneccion a bd
 main();
