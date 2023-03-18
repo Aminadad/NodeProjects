0bdd3b8020940ded8546f3335d4b3c6803016c5c
Aminadad <aminadad77@hotmail.com>
2023-03-17 23:11:43 -0400

    modify3

--- models/mascota.js
+++ models/mascota.js
@@ -5,7 +5,8 @@ const mascotaSchema = new Schema({
     nombre: String,
     descripcion: String
 });
-
+const URI = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@clusteraminadad.j8lmrfp.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+mongoose.connect(URI);
 const Mascota = mongoose.model('Mascota',mascotaSchema);
 
 module.exports = Mascota;
